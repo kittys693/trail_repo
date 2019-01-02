323da390fcc82266652163d66a66a2fc35bbe339
sravi <sravi@gmail.com>
2019-01-02 23:17:24 +0000

    added syntax error and rest

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 //prints helloworld
-        System.out.println("Hello, World");
+        System.out.println("Hello, World")
 //prints hello sravi
  System.out.println("Hello, sravi");
 System.out.println("hello anshu");
